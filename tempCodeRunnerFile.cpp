b39715c157f5f4ae2cd75a5f56611899e10a76fb
2703shivtiwari <117570696+2703shivtiwari@users.noreply.github.com>
2025-01-21 11:27:18 +0530

    volume

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,33 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class A{
 
-public:
-int Area(int l,int b)
-
+class Volume 
 {
-    return l*b;
-}
-double Area( double r)
-{
-    double const Pi = 3.14;
-    return Pi*r*r;
+    public:
+ int Volumeof(int a)
+ {
+    return a*a*a;
+ }
+ int  Volumeof(int l,int b,int h)
+ {
+    return l*b*h;
+ }
+ double Volumeof(double r,double h)
+ {
+    double const  Pi = 3.14;
+    return Pi*r*r*h;
+ }
 
-}
-float Area(float h,float b)
-{
-return 0.5f*h*b;
-
-}
 };
 int main()
 {
-    A ar;
-    cout<<"Area of rectangle : "<< ar.Area(4,8)<<endl;
-    cout<<"Area of circle : "<<ar.Area(5)<<endl;
-    cout<<"Area of Triangle : "<<ar.Area(8.0f,10.1f);
-    cout<< 'hello';
-    return 0;
 
+    Volume V;
+    cout<<"Volume of cube : "<<V.Volumeof(5)<<endl;
+    cout<<"Volume of Rectangle : "<<V.Volumeof(78,24,7)<<endl;
+    cout<<"Volume of Cylinder : "<<V.Volumeof(5.3,6.8)<<endl;
 }
